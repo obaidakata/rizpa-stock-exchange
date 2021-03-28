f4e196d321343e18e9e91426ccd164aecde9e017
Obaida kata <obaida_18@me.com>
2021-03-28 10:06:29 +0300

    + converter from generated classes to our class

--- src/app/rizpa/RizpaDataConverter.java
+++ src/app/rizpa/RizpaDataConverter.java
@@ -3,22 +3,36 @@ package app.rizpa;
 import app.rizpa.console.app.generated.RizpaStockExchangeDescriptor;
 import app.rizpa.console.app.generated.RseStock;
 import app.rizpa.console.app.generated.RseStocks;
+import app.rizpa.engine.Stock;
 import app.rizpa.engine.StockExchangeDescriptor;
+import app.rizpa.engine.Stocks;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RizpaDataConverter {
 
     public StockExchangeDescriptor convert(RizpaStockExchangeDescriptor descriptor) {
         StockExchangeDescriptor stockExchangeDescriptor = null;
+        Stocks stocks = null;
+        List<Stock> stockList = new ArrayList<>();
         if(descriptor != null) {
             RseStocks rseStocks = descriptor.getRseStocks();
             if(rseStocks != null) {
-                List<RseStock> stocks =  rseStocks.getRseStock();
-
+                List<RseStock> rseStocksList =  rseStocks.getRseStock();
+                if(rseStocksList != null) {
+                    for (RseStock stock : rseStocksList) {
+                        stockList.add(new Stock(
+                                stock.getRseSymbol(),
+                                stock.getRseCompanyName(),
+                                stock.getRsePrice()));
+                    }
+                }
             }
         }
 
+        stocks = new Stocks(stockList);
+        stockExchangeDescriptor = new StockExchangeDescriptor(stocks);
 
         return stockExchangeDescriptor;
     }
