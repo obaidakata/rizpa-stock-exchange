26dcbf5ba12dbe4657c2e1416e2221a4de624693
Ahmadsa <ahmadedu98@hotmail.com>
2021-06-03 12:24:47 +0300

    Task added

--- RizpaFX/src/app/controller/RizpaController.java
+++ RizpaFX/src/app/controller/RizpaController.java
@@ -3,6 +3,8 @@ package app.controller;
 import app.appManeger.AppManager;
 import engine.descriptor.User;
 import engine.descriptor.Users;
+import javafx.beans.binding.Bindings;
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -15,7 +17,7 @@ import rizpa.RizpaFacade;
 import java.io.File;
 import java.net.URL;
 
-public class RizpaController {
+public class RizpaController extends Task<Boolean>{
     @FXML private  ListView<String> messagesList;
 
     @FXML private Label fileStatus;
@@ -28,6 +30,13 @@ public class RizpaController {
     @FXML private AdminController adminPageController;
 
     private RizpaFacade rizpaFacade;
+
+    private final long SLEEP_TIME = 80;
+    Task<Boolean> currentRunningTask;
+    Thread thread;
+    @FXML private Label progressPercentLabel;
+    @FXML // fx:id="taskProgressBar"
+    private ProgressBar taskProgressBar; // Value injected by FXMLLoader
 
     public void setPrimaryStage(Stage primaryStage) {
         this.primaryStage = primaryStage;
@@ -45,7 +54,7 @@ public class RizpaController {
     }
 
     @FXML
-    public void loadFile(ActionEvent event){
+    public void loadFile(){
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Select Rizpa file");
         fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("xml file", "*.xml"));
@@ -56,8 +65,8 @@ public class RizpaController {
 
         String absolutePath = selectedFile.getAbsolutePath();
         try {
+            loadXML();
             rizpaFacade.loadNewData(absolutePath);
-            log("File loaded successfully");
             resetData();
             showStocks();
             showUsers();
@@ -101,5 +110,56 @@ public class RizpaController {
                 System.out.println("Resource not valid ");
             }
         }
+    }
+
+    @FXML
+    @Override
+    protected Boolean call() throws Exception {
+        updateProgress(0, 100);
+
+        for (int i = 0; i < 10; i++) {
+            Thread.sleep(SLEEP_TIME);
+            updateProgress(i + 1, 10);
+        }
+
+        return Boolean.TRUE;
+    }
+
+    public void loadXML() {
+        currentRunningTask = this;
+        bindTaskToUIComponents(currentRunningTask);
+        thread = new Thread(currentRunningTask);
+        thread.start();
+    }
+
+
+    public void bindTaskToUIComponents(Task<Boolean> aTask) {
+        // task progress bar
+        taskProgressBar.progressProperty().bind(aTask.progressProperty());
+
+        // task percent label
+        progressPercentLabel.textProperty().bind(
+                Bindings.concat(
+                        Bindings.format(
+                                "%.0f",
+                                Bindings.multiply(
+                                        aTask.progressProperty(),
+                                        100)),
+                        " %"));
+
+        // task cleanup upon finish
+        aTask.valueProperty().addListener((observable, oldValue, newValue) -> {
+            onTaskFinished();
+        });
+    }
+
+    private void onTaskFinished() {
+        this.progressPercentLabel.textProperty().unbind();
+        this.taskProgressBar.progressProperty().unbind();
+    }
+
+
+    public void loadFileButton(ActionEvent actionEvent) {
+        loadFile();
     }
 }
